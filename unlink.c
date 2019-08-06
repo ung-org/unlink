ba1abd58f61848b2bd7c54024a1ea00b82e7794d
Jakob Kaivo <jkk@ung.org>
2019-08-06 08:47:35 -0400

    rewrite

--- unlink.c
+++ unlink.c
@@ -1,31 +1,55 @@
 /*
  * UNG's Not GNU
- * 
- * Copyright (c) 2011, Jakob Kaivo <jakob@kaivo.net>
- * 
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
+ * Copyright (c) 2011-2019, Jakob Kaivo <jkk@ung.org>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
+#define _POSIX_C_SOURCE 2
+#include <errno.h>
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
-const char *unlink_desc = "call the unlink function";
-const char *unlink_inv  = "unlink file";
-
-int
-main (int argc, char **argv)
+int main(int argc, char *argv[])
 {
-  if (argc != 2)
-    return 1;
-  return unlink (argv[1]);
+	while (getopt(argc, argv, "") != -1) {
+		return 1;
+	}
+
+	if (argc - optind < 1) {
+		fprintf(stderr, "unlink: missing operand\n");
+		return 1;
+	}
+
+	if (argc - optind > 1) {
+		fprintf(stderr, "unlink: extra operand\n");
+		return 1;
+	}
+
+	if (unlink(argv[optind]) != 0) {
+		fprintf(stderr, "unlink: %s: %s\n", argv[optind],
+			strerror(errno));
+		return 1;
+	}
+
+	return 0;
 }
